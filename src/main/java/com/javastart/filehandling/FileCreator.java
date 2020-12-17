ceed561bee370f3e7ab948cb729c580b804a72bf
Rafal <piastka.rafal@gmail.com>
2020-12-17 22:18:45 +0100

    zapis/odczyt plikow

---
+++ src/main/java/com/javastart/filehandling/FileCreator.java
@@ -0,0 +1,23 @@
+package com.javastart.filehandling;
+
+import java.io.File;
+import java.io.IOException;
+
+class FileCreator {
+    public static void main(String[] args) {
+        String fileName = "testFile.txt";
+        File file = new File(fileName);
+
+        boolean fileExists = file.exists();
+        if (!fileExists) {
+            try {
+                fileExists = file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Nie udało się utworzyć pliku");
+            }
+        }
+
+        if (fileExists)
+            System.out.println("Plik " + fileName + " istnieje lub został utworzony");
+    }
+}
