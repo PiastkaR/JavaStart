fad1f622888ca3298d0dfd9ea9003911782fb1be
Rafal <piastka.rafal@gmail.com>
2020-12-31 15:00:14 +0100

    rekurencja

---
+++ src/main/java/com/javastart/recurence/DirectoryLister.java
@@ -0,0 +1,24 @@
+package com.javastart.recurence;
+
+import java.io.File;
+
+class DirectoryLister {
+    public static void main(String[] args) {
+        File file = new File("D:/");
+        showDirectories(file);
+    }
+
+    static void showDirectories(File file) {
+        File[] files = file.listFiles();
+
+        if (files != null)
+            for (File f : files) {
+                if (f.isDirectory()) {
+                    System.out.println("folder " + f.getAbsolutePath());
+                    showDirectories(f);
+                } else {
+                    System.out.println("  plik " + f.getAbsolutePath());
+                }
+            }
+    }
+}
